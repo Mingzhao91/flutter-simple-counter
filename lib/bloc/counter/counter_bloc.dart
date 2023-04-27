da126e8dd72aa2d1df270e856e9d94dd41e468a7
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2023-04-27 12:12:34 +0100

    return counter state

--- lib/bloc/counter/counter_bloc.dart
+++ lib/bloc/counter/counter_bloc.dart
@@ -1,10 +1,10 @@
-import 'package:bloc/bloc.dart';
+import '../bloc_imports.dart';
 import 'package:equatable/equatable.dart';
 
 part 'counter_event.dart';
 part 'counter_state.dart';
 
-class CounterBloc extends Bloc<CounterEvent, CounterState> {
+class CounterBloc extends HydratedBloc<CounterEvent, CounterState> {
   CounterBloc() : super(CounterInitial()) {
     on<CounterIncrementEvent>((event, emit) {
       emit(IncrementSate(state.counterValue + 1));
@@ -13,5 +13,15 @@ class CounterBloc extends Bloc<CounterEvent, CounterState> {
     on<CounterDecrementEvent>((event, emit) {
       emit(DecrementSate(state.counterValue - 1));
     });
+  }
+
+  @override
+  CounterState? fromJson(Map<String, dynamic> json) {
+    return CounterState.fromMap(json);
+  }
+
+  @override
+  Map<String, dynamic>? toJson(CounterState state) {
+    return state.toMap();
   }
 }
